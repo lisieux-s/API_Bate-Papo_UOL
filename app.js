497e9db328cbe686fd91f9e5b2f4c797773ca70a
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-01-31 14:21:25 -0300

    feat: add name validation

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import dayjs from 'dayjs';
+import joi from 'joi';
 
 const app = express();
 app.use(express.json());
@@ -16,26 +17,46 @@ mongoClient.connect(() => {
   db = mongoClient.db('bate_papo_uol');
 });
 
-let activeUser = ''
+let activeUser = '';
+
+const nameSchema = joi.string().required();
+
+const schema = joi.object({
+  to: joi.string().required(),
+  type: joi.string().required(),
+  //from: joi.string()
+});
 
 app.post('/participants', async (req, res) => {
   try {
     const name = req.body.name;
-    const participant = await db.collection('participants').insertOne({
-      name: name,
-      lastStatus: Date.now(),
-    });
+    const participantName = await db
+      .collection('participants')
+      .find({ name: name })
+      .toArray();
+    console.log(participantName);
 
-    const message = await db.collection('messages').insertOne({
-      from: name,
-      to: 'Todos',
-      text: 'entra na sala...',
-      type: 'status',
-      time: dayjs(Date.now()).format('HH:mm:ss'),
-    });
+    const validation = nameSchema.validate(name, { abortEarly: true });
+    if (validation.error) {
+      res.sendStatus(422);
+    } else if (participantName.length > 0) {
+      res.sendStatus(409);
+    } else {
+      const participant = await db.collection('participants').insertOne({
+        name: name,
+        lastStatus: Date.now(),
+      });
+      const message = await db.collection('messages').insertOne({
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: dayjs(Date.now()).format('HH:mm:ss'),
+      });
 
       activeUser = name;
-    res.sendStatus(201);
+      res.sendStatus(201);
+    }
   } catch (err) {
     console.log(err);
     res.sendStatus(err);
@@ -61,6 +82,7 @@ app.post('/messages', async (req, res) => {
     const type = req.body.type;
     const from = req.headers.user;
     const time = dayjs(Date.now()).format('HH:mm:ss');
+    //FRONT ISSUE? TIMESTAMP SOMETIMES DOES NOT APPEAR, ALONG WITH EDIT/DELETE ICONS
 
     const messages = await db.collection('messages').insertOne({
       to: to,
@@ -78,19 +100,22 @@ app.post('/messages', async (req, res) => {
 
 app.get('/messages', async (req, res) => {
   try {
-
     const messages = await db.collection('messages').find().toArray();
-    const filteredMessages = messages.filter(message => ( message.to === 'Todos' || message.to === activeUser || message.from === activeUser))
+    const filteredMessages = messages.filter(
+      (message) =>
+        message.to === 'Todos' ||
+        message.to === activeUser ||
+        message.from === activeUser
+    );
 
-    if(!req.query.limit) {
-      console.log('sem limites vei')
-      res.send(filteredMessages)
-      return
+    if (!req.query.limit) {
+      console.log('sem limites vei');
+      res.send(filteredMessages);
+      return;
     } else {
       const limit = req.query.limit;
-      res.send([...filteredMessages].reverse().slice(0, limit).reverse())
+      res.send([...filteredMessages].reverse().slice(0, limit).reverse());
     }
-
   } catch (err) {
     console.log(err);
     res.sendStatus(err);
@@ -98,6 +123,7 @@ app.get('/messages', async (req, res) => {
 });
 
 app.post('/status', (req, res) => {
+  //primeiro fazer validacao dos usuarios
 });
 
 function checkStatus() {}
